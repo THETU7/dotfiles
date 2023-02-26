66fca738044b1049277050a87e966e47762c7aa7
THETU7 <zongyang430@gmail.com>
2023-02-26 14:27:27 +0800

    add i3 wm config

---
+++ private_dot_config/polybar/grayblocks/scripts/executable_launcher.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/grayblocks/scripts/rofi/launcher.rasi
+/home/thetu/.config/rofi/launchers/type-1/launcher.sh
